5ce7215e4250ff90af81030965dc4e3e1209b323
Matias Tite <matiastite1@gmail.com>
2024-04-19 17:31:47 -0500

    Clase para crear estudiantes

---
+++ SchoolApi/models/create.php
@@ -0,0 +1,19 @@
+<?php
+include_once "connection.php";
+class Create
+{
+    public static function create()
+    {
+        $connection = Connection::connect();
+        $id = $_POST["id"];
+        $name = $_POST["name"];
+        $lastName = $_POST["lastName"];
+        $courseId = $_POST["courseId"];
+
+        $query = "INSERT INTO students (id, 'name', lastName, courseId) VALUES ($id, '$name', '$lastName', $courseId)";
+        $ans = $connection->prepare($query);
+        $ans->execute();
+
+        echo json_encode("Student created");
+    }
+}
