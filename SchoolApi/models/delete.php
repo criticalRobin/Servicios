37153855fbfa85b3c4afcb5325a25294cfcdc2eb
Matias Tite <matiastite1@gmail.com>
2024-04-19 17:57:55 -0500

    Clase para eliminar un estudiante

---
+++ SchoolApi/models/delete.php
@@ -0,0 +1,16 @@
+<?php
+include_once "connection.php";
+
+class Delete
+{
+    public static function delete()
+    {
+        $connection = Connection::connect();
+        $id = $_GET["id"];
+        $query = "DELETE FROM students WHERE id = $id";
+        $ans = $connection->prepare($query);
+        $ans->execute();
+
+        echo json_encode("Student deleted");
+    }
+}
