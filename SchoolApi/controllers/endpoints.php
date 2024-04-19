7d1270786a27cc1660fffd102cf11eb23de780c8
Matias Tite <matiastite1@gmail.com>
2024-04-19 18:03:12 -0500

    Endpoints creados

---
+++ SchoolApi/controllers/endpoints.php
@@ -0,0 +1,33 @@
+<?php
+include_once "../models/getOne.php";
+include_once "../models/getData.php";
+include_once "../models/getFiltered.php";
+include_once "../models/create.php";
+include_once "../models/delete.php";
+include_once "../models/edit.php";
+
+$opt = $_SERVER["REQUEST_METHOD"];
+
+switch ($opt) {
+    case "GET":
+        if (isset($_GET["id"])) {
+            GetOne::getOne();
+        } elseif (isset($_GET["level"]) && isset($_GET["parallel"])) {
+            GetFiltered::getFiltered();
+        } else {
+            GetData::getData();
+        }
+        break;
+    case "POST":
+        Create::create();
+        break;
+    case "DELETE":
+        Delete::delete();
+        break;
+    case "PUT":
+        Edit::edit();
+        break;
+    default:
+        echo json_encode("Method not allowed");
+        break;
+}
