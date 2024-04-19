b689e618836849790af65b11fbafb259b2195d9a
Matias Tite <matiastite1@gmail.com>
2024-04-19 17:24:16 -0500

    Clase para obtener estudiantes filtrados por curso y paralelo

---
+++ SchoolApi/models/getFiltered.php
@@ -0,0 +1,17 @@
+<?php
+class GetFiltered
+{
+    public static function getFiltered()
+    {
+        $connection = Connection::connect();
+        $level = $_GET["level"];
+        $parallel = $_GET["parallel"];
+
+        $query = "SELECT * FROM students WHERE courseId = (SELECT id FROM courses WHERE 'level' = '$level' AND parallel = '$parallel')";
+        $ans = $connection->prepare($query);
+        $ans->execute();
+        $data = $ans->fetchAll(PDO::FETCH_ASSOC);
+
+        echo json_encode($data);
+    }
+}
